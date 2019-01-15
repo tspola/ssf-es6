12ded56c84157755374f148fa027091c2335e67d
Robert Brown <33644440+robertlb4@users.noreply.github.com>
2019-01-15 15:52:12 -0800

    Update comments for added clarity

--- challenges/02-template-literals/05-stringWithObject.js
+++ challenges/02-template-literals/05-stringWithObject.js
@@ -1,6 +1,6 @@
 //Complete the below function. It will be given 3 inputs, an array of objects, an index and a key. The function should output a 
 //string that reads: The information you requested is ____.  Have a nice day!
-//Make use of interpolation to include the value of the object at the requested array index in place of the ____.
+//Make use of interpolation to include the property from the object at the requested array index in place of the ____.
 
 const stringWithObject = (array, index, key) => {
     
