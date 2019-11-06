3d22ade00985d9322d70492510f9341224848bc6
tspola <tjspolar@gmail.com>
2019-11-06 06:27:24 -0800

    loop

--- challenges/01-arrow-functions/05-fizzBuzz30.js
+++ challenges/01-arrow-functions/05-fizzBuzz30.js
@@ -2,14 +2,21 @@
 //1 through 30 replacing numbers divisible by 3 with 'fizz', numbers divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
 //'fizzbuzz'
 
-let fizzBuzz30;
 
-
-
-
-
-
-
+const fizzBuzz30 = () => {
+    let output = [];
+    for(i=1; i <= 30;  i++) {
+        output.push(
+            i%15 === 0 ? 'fizzbuzz'
+            : i%3 === 0 ? 'fizz'
+            : i%5 === 0 ? 'buzz'
+            : i
+        )
+    }
+    return output
+}
+    
+        
 
 //Do not edit
 module.exports = fizzBuzz30;
